f64c061838d0e831e686f8eb52709ef9cd9faf98
olback <git@olback.net>
2016-11-23 22:11:39 +0100

    Update youtube.php

--- youtube.php
+++ youtube.php
@@ -5,7 +5,7 @@
   <div class="content">
     <div class="container">
       <div class="main">
-        <h1><a href="https://www.youtube.com/channel/UCaLwTk-JfriVkeqqLYNQYKg/"><img src="./img/youtube.ico" width="32" height="32"></a> YouTube</h1>
+        <h1><a href="https://www.youtube.com/channel/UCaLwTk-JfriVkeqqLYNQYKg/"><!--<img src="./img/youtube.ico" width="32" height="32">--><i class="fa fa-youtube-play"></i></a> YouTube</h1>
         <hr>
         <p>
           My latest <a href="https://www.youtube.com/channel/UCaLwTk-JfriVkeqqLYNQYKg/" target="_blank">YouTube</a> videos: <br>
