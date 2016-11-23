7ca271dcdc2c99d3ed03863e3d41aaa161b2c42a
olback <git@olback.net>
2016-11-23 21:58:40 +0100

    Update projects.php

--- projects.php
+++ projects.php
@@ -7,7 +7,7 @@
       <div class="main">
         <h1>Projects</h1>
         <hr>
-        <p>First project is up on <i class="fa fa-github"></i> GitHub!</p>
+        <p>First project is up on <i class="fa fa-github"></i><a href="https://github.com/olback/olback.net">GitHub!</a></p>
       </div>
     </div>
   </div>
