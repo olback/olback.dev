5d65f7f547246a58f23982fb81cd15742f6b03bc
olback <git@olback.net>
2017-01-22 01:24:38 +0100

    Update head.php

--- res/head.php
+++ res/head.php
@@ -1,14 +1,19 @@
+<?php
+$title = ucfirst($_SESSION['fn']);
+?>
 <!DOCTYPE html>
 <html lang="en"><head>
     <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-    <title>olback.net</title>
+    <title><?php echo "$title"; ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" href="img/olback.jpg">
+    
     <!-- CSS -->
     <link rel="stylesheet" href="css/styles.css">
     <link rel="stylesheet" href="css/fa/css/font-awesome.css">
+    
     <!-- JS -->
     <script type="text/javascript" src="js/google.js"></script>
     <script type="text/javascript" src="js/jsLog.js"></script>
-	<script type="text/javascript" src="js/date-time.js"></script>
+    <script type="text/javascript" src="js/date-time.js"></script>
   </head>
