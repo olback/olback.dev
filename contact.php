24e7062d07fab3384d7eb7fee9df20a143395130
olback <git@olback.net>
2017-01-22 01:44:45 +0100

    Update contact.php

--- contact.php
+++ contact.php
@@ -40,7 +40,7 @@ endif;
 <html lang="en">
   <head>
     <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-    <title>olback.net</title>
+    <title>olback.net - Contact</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" href="./img/olback.jpg">
     <link rel="stylesheet" href="./css/styles.css">
