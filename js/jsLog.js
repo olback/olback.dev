a1cafe46a6da81517eb59ae10c9bddb28d1c6821
Edwin <git@olback.net>
2017-02-03 20:26:16 +0100

    Update jsLog.js

--- js/jsLog.js
+++ js/jsLog.js
@@ -1,10 +1,2 @@
 {
-console.log("   ▀▄   ▄▀"),
-console.log("  ▄█▀███▀█▄"),
-console.log(" █▀███████▀█"),
-console.log(" █ █▀▀▀▀▀█ █"),
-console.log("    ▀▀ ▀▀"),
-console.log("┌──────────────────────────────────────────────────────────────────┐"),
-console.log("│ Found a security issue? Please report it to security@olback.net  │"),
-console.log("└──────────────────────────────────────────────────────────────────┘")
-}
+console.log("   ▀▄   ▄▀"), console.log("  ▄█▀███▀█▄"), console.log(" █▀███████▀█"), console.log(" █ █▀▀▀▀▀█ █"), console.log("    ▀▀ ▀▀"), console.log("┌──────────────────────────────────────────────────────────────────┐"), console.log("│ Found a security issue? Please report it to security@olback.net  │"), console.log("└──────────────────────────────────────────────────────────────────┘") }
