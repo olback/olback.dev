02934848e54907b8d95d057134014b1808c2a5dd
Edwin <git@olback.net>
2017-02-04 19:20:56 +0100

    Update links.php

--- links.php
+++ links.php
@@ -15,11 +15,9 @@
           <img src="./img/ghost.ico" alt="Image not found." width="16" height="16"> GhostGamingGG <a href="http://twitch.tv/ghostgaminggg/chat/" >Chat</a> <a href="http://twitch.tv/ghostgaminggg/" >Stream</a> <br>
           <img src="./img/youtube.ico" alt="Image not found." width="16" height="16"> <a href="https://www.youtube.com/feed/subscriptions" >Youtube</a> <br>
           <img src="./img/SteamIco.ico" alt="Image not found." width="16" height="16"> Steam
-          <a href="https://steamcommunity.com/market" >Community Market</a>,
-          <a href="https://steamcommunity.com/id/olback/" >olback</a>,
+          <a href="https://steamcommunity.com/market" >Community Market</a>
+          <a href="https://steamcommunity.com/id/olback/" >olback</a>
           <a href="https://steamcommunity.com/id/xXx_PussySlayer1337_xXx/" >xXx_PussySlayer1337_xXx</a> <br>
-          <!-- <img src="./img/SteamIco.ico" alt="Image not found." width="16" height="16"> <a href="https://steamcommunity.com/id/xXx_PussySlayer1337_xXx/" >Steam</a> (xXx_PussySlayer1337_xXx)<br> -->
-          <!-- <img src="./img/SteamIco.ico" alt="Image not found." width="16" height="16"> <a href="https://steamcommunity.com/market" >Steam Community Market</a> <br> -->
           <img src="./img/ab.ico" alt="Image not found." width="16" height="16"> <a href="http://aftonbladet.se/" >Aftonbladet</a> <br>
           <img src="./img/imgur.ico" alt="Image not found." width="16" height="16"> <a href="https://imgur.com/" >Imgur</a> <br>
           <img src="./img/steamstatus.ico" alt="Image not found." width="16" height="16"> <a href="https://steamstat.us/" >Steam Status</a> <br>
