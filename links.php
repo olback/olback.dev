abc9acf25d2d5cd0d1d35ea0efa1d01e71cca37e
olback <github@olback.net>
2017-02-04 19:39:58 +0100

    updated links

--- links.php
+++ links.php
@@ -8,22 +8,16 @@
       <div class="main">
         <h1>Links</h1>
         <hr>
-        <p style="font-size:150%"> Your local date and time: <span id="date-time"> </p>
+        <p style="font-size:150%"> Your local date and time: <span id="date-time"></p>
         <hr>
         <p style="font-size:100%">
-          <img src="./img/google.ico" alt="Image not found." width="16" height="16"> <a href="https://encrypted.google.com/" >Google</a> <br>
-          <img src="./img/ghost.ico" alt="Image not found." width="16" height="16"> GhostGamingGG <a href="http://twitch.tv/ghostgaminggg/chat/" >Chat</a> <a href="http://twitch.tv/ghostgaminggg/" >Stream</a> <br>
-          <img src="./img/youtube.ico" alt="Image not found." width="16" height="16"> <a href="https://www.youtube.com/feed/subscriptions" >Youtube</a> <br>
-          <img src="./img/SteamIco.ico" alt="Image not found." width="16" height="16"> Steam
-          <a href="https://steamcommunity.com/market" >Community Market</a>
-          <a href="https://steamcommunity.com/id/olback/" >olback</a>
-          <a href="https://steamcommunity.com/id/xXx_PussySlayer1337_xXx/" >xXx_PussySlayer1337_xXx</a> <br>
-          <img src="./img/ab.ico" alt="Image not found." width="16" height="16"> <a href="http://aftonbladet.se/" >Aftonbladet</a> <br>
-          <img src="./img/imgur.ico" alt="Image not found." width="16" height="16"> <a href="https://imgur.com/" >Imgur</a> <br>
-          <img src="./img/steamstatus.ico" alt="Image not found." width="16" height="16"> <a href="https://steamstat.us/" >Steam Status</a> <br>
-          <img src="./img/lp.ico" alt="Image not found." width="16" height="16"> <a href="https://lastpass.com/" >Lastpass</a> <br>
-          <img src="./img/olback.jpg" alt="Image not found." width="16" height="16"> <a href="cheats" >olback's cheat </a> <br>
-          <img src="./img/google.ico" alt="Image not found." width="16" height="16"> <a href="https://translate.google.com/" >Google Translate</a> <br>
+          <i class="fa fa-google"></i> <a href="https://encrypted.google.com/">Google</a><br>
+          <i class="fa fa-youtube-play"></i> <a href="https://www.youtube.com/feed/subscriptions">Youtube</a><br>
+          <i class="fa fa-steam"></i> Steam <a href="https://steamcommunity.com/market">Community Market</a> <a href="https://steamcommunity.com/id/olback/">olback</a> <a href="https://steamcommunity.com/id/xXx_PussySlayer1337_xXx/">xXx_PussySlayer1337_xXx</a><br>
+          <i class="fa fa-steam"></i> <a href="https://steamstat.us/">Steam Status</a><br>
+          <i class="fa fa-lock"></i> <a href="https://lastpass.com/">Lastpass</a><br>
+          <img src="./img/olback.jpg" alt="Image not found." width="16" height="16"> <a href="cheats" >olback's cheat </a><br>
+          <i class="fa fa-google"></i> <a href="https://translate.google.com/"> Google Translate</a><br>
         </p>
       </div>
     </div>
