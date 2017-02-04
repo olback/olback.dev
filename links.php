6547ada472e802f66d8d70ad5c43b6d04485a7fd
olback <github@olback.net>
2017-02-04 19:41:54 +0100

    updated links

--- links.php
+++ links.php
@@ -11,13 +11,13 @@
         <p style="font-size:150%"> Your local date and time: <span id="date-time"></p>
         <hr>
         <p style="font-size:100%">
-          <i class="fa fa-google"></i> <a href="https://encrypted.google.com/">Google</a><br>
-          <i class="fa fa-youtube-play"></i> <a href="https://www.youtube.com/feed/subscriptions">Youtube</a><br>
-          <i class="fa fa-steam"></i> Steam <a href="https://steamcommunity.com/market">Community Market</a> <a href="https://steamcommunity.com/id/olback/">olback</a> <a href="https://steamcommunity.com/id/xXx_PussySlayer1337_xXx/">xXx_PussySlayer1337_xXx</a><br>
-          <i class="fa fa-steam"></i> <a href="https://steamstat.us/">Steam Status</a><br>
-          <i class="fa fa-lock"></i> <a href="https://lastpass.com/">Lastpass</a><br>
+          <i class="fa fa-google" style="color: blue;"></i> <a href="https://encrypted.google.com/">Google</a><br>
+          <i class="fa fa-youtube-play" style="color: red;"></i> <a href="https://www.youtube.com/feed/subscriptions">Youtube</a><br>
+          <i class="fa fa-steam" style="color: black;"></i> Steam <a href="https://steamcommunity.com/market">Community Market</a> <a href="https://steamcommunity.com/id/olback/">olback</a> <a href="https://steamcommunity.com/id/xXx_PussySlayer1337_xXx/">xXx_PussySlayer1337_xXx</a><br>
+          <i class="fa fa-steam" style="color: black;"></i> <a href="https://steamstat.us/">Steam Status</a><br>
+          <i class="fa fa-lock" style="color: green;"></i> <a href="https://lastpass.com/">Lastpass</a><br>
           <img src="./img/olback.jpg" alt="Image not found." width="16" height="16"> <a href="cheats" >olback's cheat </a><br>
-          <i class="fa fa-google"></i> <a href="https://translate.google.com/"> Google Translate</a><br>
+          <i class="fa fa-google" style="color: blue;"></i> <a href="https://translate.google.com/"> Google Translate</a><br>
         </p>
       </div>
     </div>
