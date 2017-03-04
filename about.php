432845082cf7a9755ebf29a0cae67e3939308639
Edwin <git@olback.net>
2017-03-05 00:58:37 +0100

    Update about.php

--- about.php
+++ about.php
@@ -8,7 +8,12 @@
       <div class="main">
         <h1>About</h1>
         <hr>
-        <p></p>
+        <p>
+        <h3>Who are you?</h3>
+        I'm a student from Sweden and I study engineering/IT at the moment. I really like maths and physics, don't really know why.
+        In my spare time I like to design and develop websites as well as phoyography. Can't really say I'm good at it but I'm learning and it's a whole lot of fun.
+          
+        </p>
       </div>
     </div>
   </div>
