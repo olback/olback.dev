b073293fa7c862ea241a6bc1d6166bf9563a7690
Edwin <git@olback.net>
2017-03-05 01:17:55 +0100

    Update contact.php

--- contact.php
+++ contact.php
@@ -93,7 +93,7 @@ endif;
   	  </div>
           <div class="r">
           <center>
-           <p style="margin: 10px; right: 0;">
+           <p style="margin: 10px; margin-top: 50px;">
 	    If you'd like to contact me, please use the contact form below. Messages are NOT encrypted. Do not send any sensitive information.
 	   </p>
            <br><br><br><br><br><br><br><br><br><br><br><br><br><br>
