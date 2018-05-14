6a67824526b994a15f6e7385daf64f463bf8652c
olback <git@olback.net>
2018-05-14 17:39:22 +0200

    sample config

---
+++ src/sample.conf.rs
@@ -0,0 +1,6 @@
+pub const HOST: &str = ""; // Mail server hostname
+pub const USER: &str = ""; // Mail user name
+pub const PASS: &str = ""; // Mail password
+pub const FROM: &str = ""; // From address
+pub const NAME: &str = ""; // From name
+pub const SITE: &str = "olback.net"; // Root domain
