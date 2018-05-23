bc54b6fc4ec8d69ca328b1e1013e710c2b228be7
Edwin Svensson <git@olback.net>
2018-05-23 11:19:03 +0200

    add check() to sample.conf.rs

--- src/sample.conf.rs
+++ src/sample.conf.rs
@@ -4,3 +4,42 @@ pub const PASS: &str = ""; // Mail password
 pub const FROM: &str = ""; // From address
 pub const NAME: &str = ""; // From name
 pub const SITE: &str = "olback.net"; // Root domain
+
+pub fn check() -> bool {
+
+    let mut pass = true;
+
+    if HOST.is_empty() {
+        println!("{}", "Field 'HOST' is empty!".bold().yellow());
+        pass = false;
+    }
+
+    if USER.is_empty() {
+        println!("{}", "Field 'USER' is empty!".bold().yellow());
+        pass = false;
+    }
+
+    if PASS.is_empty() {
+        println!("{}", "Field 'PASS' is empty!".bold().yellow());
+        pass = false;
+    }
+
+    if FROM.is_empty() {
+        println!("{}", "Field 'FROM' is empty!".bold().yellow());
+        pass = false;
+    }
+
+    if NAME.is_empty() {
+        println!("{}", "Field 'NAME' is empty!".bold().yellow());
+        pass = false;
+    }
+
+    if SITE.is_empty() {
+        println!("{}", "Field 'SITE' is empty!".bold().yellow());
+        pass = false;
+    }
+
+    pass
+
+}
+
