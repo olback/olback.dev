d97205015ffceffe418c0d7d976553bb69744afd
Edwin Svensson <git@olback.net>
2019-10-18 04:09:29 +0200

    attempt 100?

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-CIRCLE_BUILD_NUM=190
-CIRCLE_NODE_INDEX=0
-# URL="https://olback.net/update"
+URL="https://olback.net/update"
 
 JSON="{\"build_num\": \"$CIRCLE_BUILD_NUM\",\"node_index\": \"$CIRCLE_NODE_INDEX\"}"
 
